2c3769eb85c7a5725655854a69de99c51f90aaf9
Lucas Rodrigues <lucasrdoficial@gmail.com>
2017-06-07 11:14:00 -0300

    Primeira questão com bonus e segunda questão feitas - Falta Jupyter

---
+++ Segundo Trabalho/src/draw.py
@@ -0,0 +1,37 @@
+import teo1
+import teo1bonus
+import tsp_reader
+import networkx as nx
+import matplotlib.pyplot as plt
+import sys
+
+theorem = int(sys.argv[1])
+filename = sys.argv[2]
+city = filename.split('/')[-1].split('.')[0]
+
+fn = None
+if theorem == 1:
+  fn = teo1.teo_1
+else:
+  fn = teo1bonus.teo_1bonus
+
+g = tsp_reader.read(open(filename).read())
+
+for k in range(1, len(g.nodes())+1):
+  f = fn(g, k)
+
+  n = nx.Graph()
+
+  print k
+  for edge in f.edges():
+    print "%d %d %d" % (edge[0], edge[1], g.edge_weight(edge))
+  print
+
+  for node in f.nodes():
+    n.add_node(node)
+
+  for edge in f.edges():
+    n.add_edge(edge[0], edge[1])
+  nx.draw(n)
+  plt.savefig('figs/teo%d/%s-k%d.png' % (theorem, city, k))
+  plt.clf()
